fb8e3ab10fbe7dce3b76e1ce0a41759e256d5162
Renziito <sepia.aki@gmail.com>
2019-07-23 22:45:54 +0000

    🍵⚡️ Checkpoint ./bot.js:5674830/1314 ./package.json:5674830/27 ./watch.json:5674830/16 ./commands/avatar.js:5674830/658

--- bot.js
+++ bot.js
@@ -1,15 +1,63 @@
 var Discord = require('discord.js');
 var logger = require('winston');
+var fs = require('fs');
+var prefix = 'd!';
+
+var bot = new Discord.Client({disableEveryone: true});
+
+
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
   colorize: true
 });
+
 logger.level = 'debug';
-var bot = new Discord.Client();
+
+
+bot.commands = new Discord.Collection();
+
 bot.on('ready', function() {
   logger.info('Connected');
 });
+
+fs.readdir("./commands", (err, files) => {
+    if(err) console.error(err);
+
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0) {
+        console.log("No commands found to load!");
+        return;
+    }
+
+    console.log(`Loading ${jsfiles.length} commands!`);
+
+    jsfiles.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${i + 1}: ${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
+});
+
 bot.login(process.env.TOKEN);
+
+
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;  
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+
+    let cmd = bot.commands.get(command.slice(prefix.length));
+    if(cmd) cmd.run(bot, message, args);
+    
+});
+
+
 bot.on('message', function(msg){
   if (msg.content.substring(0, 2) == 'd!') {
     var args = msg.content.substr(msg.content.indexOf(" ") + 1);;
@@ -17,9 +65,6 @@ bot.on('message', function(msg){
     var embed = new Discord.RichEmbed();
     args= args=='d!'+cmd?"":args;
     switch(cmd) {
-      case 'ping':
-        msg.channel.send("Pong!");
-        break;
       case 'duele':
         msg.reply("Me dueles, me hieres, me lastimas!");
         break;
@@ -53,6 +98,7 @@ bot.on('message', function(msg){
     }
   }
 });
+
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
