d18269200baa1aa86bc06b1614ea639456bfd94f
Renziito <sepia.aki@gmail.com>
2019-07-23 18:48:34 +0000

    first commit

---
+++ bot.js
@@ -0,0 +1,92 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+
+logger.level = 'debug';
+
+var bot = new Discord.Client({
+   token: process.env.TOKEN,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: '+bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message.substring(0, 2) == 'd!') {
+        var args = message.substr(message.indexOf(" ") + 1);;
+        var cmd = message.substring(2).split(' ')[0];
+        var serverID = evt.d.guild_id;
+        var isMod = bot.servers[serverID].members[userID].roles.includes('603231899279949846');
+      
+        logger.info(cmd);
+            
+        switch(cmd) {
+            case 'ping':
+                bot.sendMessage({to: channelID,message: 'Pong!'});
+            break;
+            
+          case 'duele':
+            bot.sendMessage({
+                    to: channelID,
+                    message: user + ' me dueles, me hieres, me lastimas!'
+                });
+            break;
+               
+          case 'fuerte':
+            bot.sendMessage({to: channelID,message: 'Ay que Fuerte!'});
+            break;
+                
+          case 'amo':
+            bot.sendMessage({to: channelID,message: 'Amo su inocencia 17 años'});
+            break;
+                 
+          case 'redes':
+            bot.sendMessage({
+                    to: channelID,
+                    message: '¡Sígueme en mis RRSS! (´∀`ゞ ❤ instagram.com/dahianaly ❤ facebook.com/dahianaly ❤ twitter.com/dahianaly'
+                });
+            break;
+            
+          case 'cumple':
+            bot.sendMessage({
+                    to: channelID,
+                    message: '¡Feliz cumpleaños '+ args +' !'
+                });
+            bot.sendMessage({
+              to: channelID,
+              message: "https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2F_3fYL8i6Q-n-155t3dn_4hOC19Na98jFfWcrc8s1mCV2b1zjavufTWsmpUZjCrMO.gif?v=1563894896587"
+            });
+            break;  
+            
+          case 'say':
+            //Dango Moderador
+            if(isMod){
+              bot.sendMessage({to: channelID,message: args});
+            }else{
+              bot.sendMessage({to: channelID,message: user+" no puedes usar ese comando :P"});
+            }
+            
+            
+            break;
+         }
+      
+      bot.deleteMessage({channelID:channelID,messageID:evt.d.id})
+      
+     }
+});
+
+
+const express = require('express');
+const app = express();
+ 
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
