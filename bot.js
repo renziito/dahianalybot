be7f7e8bc4cd5c747427fcd76378b67cfed9ae7d
Renziito <sepia.aki@gmail.com>
2019-07-23 22:34:54 +0000

    🍔🙉 Checkpoint ./commands/ping.js:5674830/389 ./bot.js:5674830/8139

--- bot.js
+++ bot.js
@@ -1,78 +1,60 @@
 var Discord = require('discord.js');
 var logger = require('winston');
-
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+  colorize: true
 });
-
 logger.level = 'debug';
-
 var bot = new Discord.Client();
-
 bot.on('ready', function() {
-    logger.info('Connected');
+  logger.info('Connected');
 });
-
 bot.login(process.env.TOKEN);
-
 bot.on('message', function(msg){
   if (msg.content.substring(0, 2) == 'd!') {
     var args = msg.content.substr(msg.content.indexOf(" ") + 1);;
     var cmd = msg.content.substring(2).split(' ')[0];
     var embed = new Discord.RichEmbed();
-    
     args= args=='d!'+cmd?"":args;
-    
     switch(cmd) {
       case 'ping':
         msg.channel.send("Pong!");
         break;
-
       case 'duele':
         msg.reply("Me dueles, me hieres, me lastimas!");
         break;
-
       case 'fuerte':
         msg.channel.send("Ay que Fuerte!");
         break;
-
       case 'amo':
         msg.channel.send("Amo su inocencia 17 años");
         break;
-
       case 'redes':
         msg.channel.send("¡Sígueme en mis RRSS! (´∀`ゞ ❤ instagram.com/dahianaly ❤ facebook.com/dahianaly ❤ twitter.com/dahianaly");
         break;
-
       case 'cumple':
         embed.setTitle("¡Feliz Cumpleaños!!")
-              .setColor(0x00AE86)
-              .setDescription(args + "te deseamos un muy feliz cumpleaños!! Pasala genial!!.")
-              .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fcumple.gif?v=1563915113443")
+          .setColor(0x00AE86)
+          .setDescription(args + "te deseamos un muy feliz cumpleaños!! Pasala genial!!.")
+          .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fcumple.gif?v=1563915113443")
         msg.channel.send(embed);
         break;  
-
       case 'kawaii':
         embed.setTitle("Kawaii")
-              .setColor(0x00AE86)
-              .setDescription("Intentando ser Kawaii")
-              .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fkawaii.png?v=1563918163191")
+          .setColor(0x00AE86)
+          .setDescription("Intentando ser Kawaii")
+          .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fkawaii.png?v=1563918163191")
         msg.channel.send(embed);
         break;  
-
       case 'say':
         //Dango Moderador
         msg.channel.send(args);
         break;
-     }
+    }
   }
 });
-
-
 const express = require('express');
 const app = express();
- 
 app.get("/", (request, response) => {
   response.sendStatus(200);
 });
--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,7 @@
 module.exports.run = async (bot, message, args) => {
-message.channel.send('Pong! `' + Math.floor(Math.round(bot.ping)) + '`ms')
-
-message.delete();
-
+  message.channel.send('Pong! `' + Math.floor(Math.round(bot.ping)) + '`ms')
+  message.delete();
 }
-  module.exports.help = {
-    name: "ping"
-  }
+module.exports.help = {
+  name: "ping"
+}
