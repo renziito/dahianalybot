ebed23b30aa93e57c6d84b51d5573e32f2e3a58c
Renziito <sepia.aki@gmail.com>
2019-07-23 21:52:54 +0000

    🏊🏼👙 Checkpoint ./.glitch-assets:5674830/417 ./bot.js:5674830/176 ./watch.json:5674830/2

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ bot.on('message', function(msg){
         embed.setTitle("Kawaii")
               .setColor(0x00AE86)
               .setDescription("Intentando ser Kawaii")
-              .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fkawaii.MOV?v=1563917468334")
+              .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fkawaii.png?v=1563918163191")
         msg.channel.send(embed);
         break;  
 
