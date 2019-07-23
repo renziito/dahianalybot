4639d97973934f3104600cf72111d095702d7c1a
Renziito <sepia.aki@gmail.com>
2019-07-23 21:41:56 +0000

    ⏳💎 Checkpoint ./.glitch-assets:5674830/346 ./bot.js:5674830/2671 ./watch.json:5674830/610 ./package.json:5674830/181

--- bot.js
+++ bot.js
@@ -10,55 +10,62 @@ logger.level = 'debug';
 
 var bot = new Discord.Client();
 
-bot.on('ready', ()=> {
+bot.on('ready', function() {
     logger.info('Connected');
 });
 
 bot.login(process.env.TOKEN);
 
-bot.on('message', (msg)=>{
+bot.on('message', function(msg){
   if (msg.content.substring(0, 2) == 'd!') {
     var args = msg.content.substr(msg.content.indexOf(" ") + 1);;
     var cmd = msg.content.substring(2).split(' ')[0];
+    var embed = new Discord.RichEmbed();
     
     args= args=='d!'+cmd?"":args;
     
     switch(cmd) {
-          case 'ping':
-            msg.channel.send("Pong!");
-            break;
-            
-          case 'duele':
-            msg.reply("Me dueles, me hieres, me lastimas!");
-            break;
-               
-          case 'fuerte':
-            msg.channel.send("Ay que Fuerte!");
-            break;
-                
-          case 'amo':
-            msg.channel.send("Amo su inocencia 17 años");
-            break;
-                 
-          case 'redes':
-            msg.channel.send("¡Sígueme en mis RRSS! (´∀`ゞ ❤ instagram.com/dahianaly ❤ facebook.com/dahianaly ❤ twitter.com/dahianaly");
-            break;
-            
-          case 'cumple':
-            const embed = new Discord.RichEmbed()
-                  .setTitle("¡Feliz Cumpleaños!!")
-                  .setColor(0x00AE86)
-                  .setDescription(args + "te deseamos un muy feliz cumpleaños!! Pasala genial!!.")
-                  .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fcumple.gif?v=1563915113443")
-            msg.channel.send(embed);
-            break;  
-            
-          case 'say':
-            //Dango Moderador
-            msg.channel.send(args);
-            break;
-         }
+      case 'ping':
+        msg.channel.send("Pong!");
+        break;
 
+      case 'duele':
+        msg.reply("Me dueles, me hieres, me lastimas!");
+        break;
+
+      case 'fuerte':
+        msg.channel.send("Ay que Fuerte!");
+        break;
+
+      case 'amo':
+        msg.channel.send("Amo su inocencia 17 años");
+        break;
+
+      case 'redes':
+        msg.channel.send("¡Sígueme en mis RRSS! (´∀`ゞ ❤ instagram.com/dahianaly ❤ facebook.com/dahianaly ❤ twitter.com/dahianaly");
+        break;
+
+      case 'cumple':
+        embed.setTitle("¡Feliz Cumpleaños!!")
+              .setColor(0x00AE86)
+              .setDescription(args + "te deseamos un muy feliz cumpleaños!! Pasala genial!!.")
+              .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fcumple.gif?v=1563915113443")
+        msg.channel.send(embed);
+        break;  
+
+      case 'kawaii':
+        embed.setTitle("Kawaii")
+              .setColor(0x00AE86)
+              .setDescription("Intentando ser Kawaii")
+              .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fkawaii.MOV?v=1563917468334")
+        msg.channel.send(embed);
+        break;  
+
+      case 'say':
+        //Dango Moderador
+        msg.channel.send(args);
+        break;
+     }
   }
 });
 
