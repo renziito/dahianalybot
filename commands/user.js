4d5e04dc16674f6ff3045a7c818ed00a1179b160
Renziito <sepia.aki@gmail.com>
2019-07-24 14:03:06 +0000

    😜👻 Checkpoint ./.glitch-assets:5674830/885 ./commands/user.js:5674830/317 ./watch.json:5674830/2

--- commands/user.js
+++ commands/user.js
@@ -4,17 +4,17 @@ module.exports.run =async (bot, message, args) => {
     let inline = true
     let resence = true
     const status = {
-        online: "<:online:424890369688469504> Online",
-        idle: "<:idle:424890472855502849> Idle",
-        dnd: "<:dnd:424890429524410368> Do Not Disturb",
-        offline: "<:offilne:424890400319340546> Offline/Invisible"
+        online: "✅ Online",
+        idle: "🕘 Idle",
+        dnd: "⛔ No Molestar",
+        offline: "🚫 Offline/Invisible"
       }
         
     const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
     let target = message.mentions.users.first() || message.author
 
     if (member.user.bot === true) {
-      bot = "<:bottag:425631858265423883> Yes";
+      bot = "🤖 Si";
     } else {
       bot = "<:user:424958082691629057> No";
     }
@@ -24,9 +24,7 @@ module.exports.run =async (bot, message, args) => {
         .setThumbnail((target.displayAvatarURL))
         .setColor("#00ff00")
         .addField("Username", `${member.user.tag}`, inline)
-        .addField("ID", member.user.id, inline)
-        .addField("Nickname", `${member.nickname !== null ? `<:yes:425632265993846795> Nickname: ${member.nickname}` : " None"}`, true)
-        .addField("Bot", `${bot}`,inline, true)
+        .addField("Nickname", `${member.nickname !== null ? `👍 Nickname: ${member.nickname}` : "🙅 No tiene"}`, true)
         .addField("Estado", `${status[member.user.presence.status]}`, inline, true)
         .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "<:no:425632070036094986> No Roles"}`, true)
         .addField("Se unio a Joined ", member.user.createdAt)
