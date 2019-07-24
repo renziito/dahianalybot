893cb39ea948a59cc20fcc5337d642c7cad47b5b
Renziito <sepia.aki@gmail.com>
2019-07-24 15:07:13 +0000

    🌽📡 Checkpoint ./commands/user.js:5674830/437 ./watch.json:5674830/6

--- commands/user.js
+++ commands/user.js
@@ -3,35 +3,38 @@ const moment = require("moment");
 const tz= require("moment-timezone");
 
 module.exports.run =async (bot, message, args) => {
-    let inline = true
-    let resence = true
-    const status = {
-        online: "✅ Online",
-        idle: "🕘 Idle",
-        dnd: "⛔ No Molestar",
-        offline: "🚫 Offline/Invisible"
-      }
-        
-    const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
-    let target = message.mentions.users.first() || message.author
-
-    if (member.user.bot === true) {
-      bot = "🤖 Si";
-    } else {
-      bot = "🙅 No";
+  let inline = true
+  let resence = true
+  const status = {
+      online: "✅ Online",
+      idle: "🕘 Idle",
+      dnd: "⛔ No Molestar",
+      offline: "🚫 Offline/Invisible"
     }
+
+  const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+  const guildMember = message.guild.members.get(member.user.id);
+  let target = message.mentions.users.first() || message.author
+
+  if (member.user.bot === true) {
+    bot = "🤖 Si";
+  } else {
+    bot = "🙅 No";
+  }
   
   moment.locale('es');
   
-  console.log(member.user.joinedAt);
   
-  let fecha = moment(member.user.joinedAt).tz('America/Lima').format("dddd, MMMM Do YYYY, h:mm:ss a");
+  
+  console.log(guildMember.joinedAt);
+  console.log(guildMember.joinedTimestamp);
+  
+  let fecha = moment(guildMember.joinedAt).tz('America/Lima').format("dddd, MMMM Do YYYY, h:mm:ss a");
 
     let embed = new Discord.RichEmbed()
         .setAuthor("Información de "+ member.user.username)
         .setThumbnail((target.displayAvatarURL))
         .setColor("#00ff00")
-        .addField("Username", `${member.user.tag}`, inline)
         .addField("Estado", `${status[member.user.presence.status]}`, inline, true)
         .addField("Nickname", `${member.nickname !== null ? `👍 ${member.nickname}` : "🙅 No tiene"}`, true)
         .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "🙅 Sin Roles"}`, true)
