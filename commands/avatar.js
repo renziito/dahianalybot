37da3ef59896fe8b052c3a8739e353d73c5719cc
Renziito <sepia.aki@gmail.com>
2019-07-24 04:50:09 +0000

    commands

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,20 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let msg = await message.channel.send("Generating avatar...");
-
     let mentionedUser = message.mentions.users.first() || message.author;
-  
-    console.log(mentionedUser.username);
-    console.log(mentionedUser.member);
-    //let name = mentionedUser.member.nickname || mentionedUser.username;
 
     let embed = new Discord.RichEmbed()
-
-    .setImage(mentionedUser.displayAvatarURL)
-    .setColor("00ff00")
-    .setTitle("Avatar de "+mentionedUser.username)
-    .setFooter("Pedido Por " + message.author.tag)
-    .setDescription("[Avatar URL link]("+mentionedUser.displayAvatarURL+")");
+      .setImage(mentionedUser.displayAvatarURL)
+      .setColor("00ff00")
+      .setTitle("Avatar de "+mentionedUser.username)
+      .setFooter("Pedido Por " + message.author.tag)
+      .setDescription("[Avatar URL link]("+mentionedUser.displayAvatarURL+")");
 
     message.channel.send(embed)
 }
--- commands/fuerte.js
+++ commands/fuerte.js
@@ -1,5 +1,11 @@
+var Discord = require('discord.js');
+var embed = new Discord.RichEmbed();
+
 module.exports.run = async (bot, message, args) => {
-  message.channel.send('Ay que fuerte!')
+  embed.setTitle("Ay que fuerte!")
+    .setColor(0x00AE86)
+    .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fay_q.jpg?v=1563943544275")
+  message.channel.send(embed);
 }
 module.exports.help = {
   name: "fuerte"
