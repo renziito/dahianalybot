41165f042eee4cc48d5560daeb4c8eb2b876eb96
Renziito <sepia.aki@gmail.com>
2019-07-24 04:41:56 +0000

    📇💖 Checkpoint ./commands/avatar.js:5674830/522 ./watch.json:5674830/8

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,19 +4,20 @@ module.exports.run = async (bot, message, args) => {
     let msg = await message.channel.send("Generating avatar...");
 
     let mentionedUser = message.mentions.users.first() || message.author;
+  
+    console.log(mentionedUser.username);
+    console.log(mentionedUser.member);
+    //let name = mentionedUser.member.nickname || mentionedUser.username;
 
-        let embed = new Discord.RichEmbed()
+    let embed = new Discord.RichEmbed()
 
-        .setImage(mentionedUser.displayAvatarURL)
-        .setColor("00ff00")
-        .setTitle("Avatar de "+mentionedUser)
-        .setFooter("Pedido Por " + message.author.tag)
-        .setDescription("[Avatar URL link]("+mentionedUser.displayAvatarURL+")");
+    .setImage(mentionedUser.displayAvatarURL)
+    .setColor("00ff00")
+    .setTitle("Avatar de "+mentionedUser.username)
+    .setFooter("Pedido Por " + message.author.tag)
+    .setDescription("[Avatar URL link]("+mentionedUser.displayAvatarURL+")");
 
-        message.channel.send(embed)
-
-
-    msg.delete();
+    message.channel.send(embed)
 }
 
 module.exports.help = {
