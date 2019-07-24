e7da1475b0110ee7ae6f4c929066e2b26eb2e4f4
Renziito <sepia.aki@gmail.com>
2019-07-24 05:02:58 +0000

    ⏲🏪 Checkpoint ./README.md:5674830/138 ./commands/cumple.js:5674830/162 ./watch.json:5674830/2

--- commands/cumple.js
+++ commands/cumple.js
@@ -2,9 +2,10 @@ var Discord = require('discord.js');
 var embed = new Discord.RichEmbed();
 
 module.exports.run = async (bot, message, args) => {
+  let mentionedUser = message.mentions.users.first() || message.author;
   embed.setTitle("¡Feliz Cumpleaños!!")
     .setColor(0x00AE86)
-    .setDescription(args + "te deseamos un muy feliz cumpleaños!! Pasala genial!!.")
+    .setDescription(mentionedUser + " te deseamos un muy feliz cumpleaños!! Pasala genial!!.")
     .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fcumple.gif?v=1563915113443")
   message.channel.send(embed);
 }
