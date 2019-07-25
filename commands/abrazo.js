8647722ef3821b70966dbfa19e20376b5f881f6e
Renziito <sepia.aki@gmail.com>
2019-07-25 14:01:00 +0000

    🎊💼 Checkpoint ./commands/abrazo.js:5674830/472

--- commands/abrazo.js
+++ commands/abrazo.js
@@ -1,14 +1,24 @@
 const Discord = require("discord.js");
+const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
    let { body } = await superagent.get("https://firefiles.herokuapp.com/hug")
    let link = body.url 
-  const embed = new Discord.RichEmbed() 
+   let target = message.mentions.users.first() || message.author
+   let msg = " ";
+  
+   if (target){
+     msg = 
+   }else{
+     msg = "Ten un abrazo de mi parte, " +message.author+" OwO"
+   }
+   
+   const embed = new Discord.RichEmbed() 
       .setDescription(message.author+" ha empezado a fumar.")
       .setColor("RANDOM")
       .setImage(link)
       .setFooter("Powered by: FireFiles")
-  message.channel.send(embed) 
+    message.channel.send(embed) 
 }
 module.exports.help = {
   name: "abrazo"
