1f5afe31bbf199b10c103404be7300c460a0ec84
Renziito <sepia.aki@gmail.com>
2019-07-25 23:52:14 +0000

    🐿💼 Checkpoint ./commands/antas.js:5674830/9 ./watch.json:5674830/6 ./commands/infiel.js:5674830/743 ./commands/aea.js:5674830/646

---
+++ commands/aea.js
@@ -0,0 +1,7 @@
+
+module.exports.run = async (bot, message, args) => {
+  message.channel.send('<:aea:599294511721480202> <:aea:599294511721480202> <:aea:599294511721480202> <:aea:599294511721480202> <:aea:599294511721480202>')
+}
+module.exports.help = {
+  name: "aea"
+}
--- commands/antas.js
+++ commands/antas.js
@@ -3,5 +3,5 @@ module.exports.run = async (bot, message, args) => {
   message.channel.send('<:antas:595960952877350922> <:antas:595960952877350922> <:antas:595960952877350922><:antas:595960952877350922> <:antas:595960952877350922>')
 }
 module.exports.help = {
-  name: "ping"
+  name: "antas"
 }
