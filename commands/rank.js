a750d8a1c04b4717fee031e4d51e3a6ec030a2a0
Renziito <sepia.aki@gmail.com>
2019-07-25 20:34:43 +0000

    🍖🏟 Checkpoint ./commands/rank.js:5674830/403 ./watch.json:5674830/8

--- commands/rank.js
+++ commands/rank.js
@@ -4,13 +4,18 @@ const assert = require('assert');
 
 module.exports.run = async (bot, message, args) => {
   
+  var logger = require('winston');
+  logger.info(message.client.rest.client)
+   message.delete();
+  return;
+  
   const uri = "mongodb+srv://dahianalybot:sarabia@dahianalybot-lgufd.mongodb.net/test?retryWrites=true&w=majority";
   const client = new MongoClient(uri, { useNewUrlParser: true });
   
   client.connect(err => {
     const collection = client.db("dango").collection("ranks");
-    let 
-    console.log(collection.db);
+    let target = message.author.id;
+    console.log(collection.db[target]);
     client.close();
   });
 
