031cfb22e0b14e0730e7e624dcfa34cb08edfe7b
Renziito <sepia.aki@gmail.com>
2019-07-26 15:02:35 +0000

    🌒🛫 Checkpoint ./commands/infiel.js:5674830/29 ./commands/duele.js:5674830/487 ./.glitch-assets:5674830/715 ./watch.json:5674830/4 ./commands/antafuera.js:5674830/614

--- commands/antafuera.js
+++ commands/antafuera.js
@@ -2,12 +2,11 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
   let emb = new Discord.RichEmbed()
-  .setTitle('Me dueles, me hieres, me lastimas!')
   .setColor("RANDOM")
-  .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fduele.jpg?v=1564153188052")
+  .setImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Ffuera.jpg?v=1564153257875")
 
   message.channel.send(emb)
 }
 module.exports.help = {
-  name: "duele"
+  name: "antafuera"
 }
--- commands/infiel.js
+++ commands/infiel.js
@@ -1,8 +1,6 @@
-
 module.exports.run = async (bot, message, args) => {
   message.channel.send('🎵 Porque eres un estúpido, Cupido, presumido, torpe que no tienes agallas, que dices mentiras baratas que no sabes como es que se trata a una mujer como yooo... 🎵')
 }
 module.exports.help = {
   name: "infiel"
 }
-//<:aea:599294511721480202>
