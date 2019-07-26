068cfddce2785532206de517fb25df24313820de
Renziito <sepia.aki@gmail.com>
2019-07-26 15:13:33 +0000

    💳🚈 Checkpoint ./commands/tabien.js:5674830/335 ./watch.json:5674830/2

---
+++ commands/tabien.js
@@ -0,0 +1,7 @@
+//
+module.exports.run = async (bot, message, args) => {
+  message.channel.send('Ta bien pues, que te dure <:okk:586723503860875265>')
+}
+module.exports.help = {
+  name: "tabien"
+}
