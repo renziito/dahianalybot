3187621dc65020f68b24533f66d77b1646872347
Renziito <sepia.aki@gmail.com>
2019-07-29 23:23:48 +0000

    🕷🌹 Checkpoint ./watch.json:5674830/16 ./commands/rip.js:5674830/732

--- commands/rip.js
+++ commands/rip.js
@@ -7,29 +7,43 @@ module.exports.run = async (bot, message, args) => {
   const mentionedUser = message.mentions.users.first() || message.author;
   const avatarURL = mentionedUser.displayAvatarURL;
   
+  let mensaje = "";
+
+  if (args.length === 0) {
+    mensaje = "1993 - 2019"; 
+  }
+  
+  
+  if (args.length === 1) {
+    mensaje =  args.toString()
+  } else {
+    mensaje =  args.join(" ");
+  }
+  
+  console.log(msg);
   try{
     const base = await loadImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Frip.png?v=1564422037717");
     const avatar = await loadImage(avatarURL);
    
-    const canvas = createCanvas(250, 250);
+    const canvas = createCanvas(250, 376);
     const ctx = canvas.getContext('2d');
 
     ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
-	  ctx.drawImage(avatar, 80, 60, 100, 100);
+	  ctx.drawImage(avatar, 80, 100, 100, 100);
     
-    const data = ctx.getImageData(80, 60, 100, 100);
+    const data = ctx.getImageData(80, 100, 100, 100);
 		for (let i = 0; i < data.data.length; i += 4) {
 			const brightness = (0.34 * data.data[i]) + (0.5 * data.data[i + 1]) + (0.16 * data.data[i + 2]);
 			data.data[i] = brightness;
 			data.data[i + 1] = brightness;
 			data.data[i + 2] = brightness;
 		}
-		ctx.putImageData(data, 80, 60);
+		ctx.putImageData(data, 80, 100);
     
     ctx.font = "bold 14px sans-serif";
     ctx.textAlign = "center"; 
-    ctx.fillText(mentionedUser.username, canvas.width/2, 30);
-    ctx.fillText("1993 - 2019", canvas.width/2, 180);
+    ctx.fillText(mentionedUser.username, canvas.width/2, 40);
+    ctx.fillText("1993 - 2019", canvas.width/2, 230);
     
     const attachment = new Discord.Attachment(canvas.toBuffer(), 'rip-image.png');
     msg.delete();
