37b44d47eb2bcfaeb0def0b1c95a6104d2b90346
Renziito <sepia.aki@gmail.com>
2019-07-29 17:46:50 +0000

    ⚓️🏦 Checkpoint ./commands/rip.js:5674830/2568 ./.glitch-assets:5674830/367 ./package.json:5674830/1209 ./watch.json:5674830/4

--- commands/rip.js
+++ commands/rip.js
@@ -1,16 +1,35 @@
 const Discord = require('discord.js');
 const { createCanvas, loadImage } = require("canvas");
+const request = require('node-superfetch');
 
 module.exports.run = async (bot, message, args) => {
-  const base = await loadImage();
-  const canvas = createCanvas(base.width, base.height);
-  const ctx = canvas.getContext('2d');
-  let emb = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setImage("https://media.giphy.com/media/pUeXcg80cO8I8/source.gif")
+  let msg = await message.channel.send("Generating avatar...");
+  const mentionedUser = message.mentions.users.first() || message.author;
+  const avatarURL = mentionedUser.displayAvatarURL;
+  
+  try{
+    const canvas = createCanvas(300, 300);
+    const ctx = canvas.getContext('2d');
 
-  message.channel.send(emb)
+    const { rip }  = await request.get(avatarURL)
+    const base = await loadImage(rip);
+
+    const { body } = await request.get(avatarURL);
+    const avatar = await loadImage(body);
+
+    ctx.drawImage(base, 0, 0);
+    ctx.drawImage(avatar, 194, 399, 500, 500);
+
+    const emb = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setImage(canvas.toBuffer())
+
+    return  message.channel.send(emb)
+  }catch(err) {
+    return  message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+  }
+ 
 }
 module.exports.help = {
-  name: "searmo"
+  name: "rip"
 }
