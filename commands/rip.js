0e8e3eb0e791c933f1a985e1d2477ea6bfe1b4ba
Renziito <sepia.aki@gmail.com>
2019-07-29 18:08:48 +0000

    🐮👲 Checkpoint ./commands/rip.js:5674830/100 ./watch.json:5674830/2

--- commands/rip.js
+++ commands/rip.js
@@ -9,18 +9,18 @@ module.exports.run = async (bot, message, args) => {
   
   try{
     const base = await loadImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Frip.png?v=1564422037717");
-    
+   
     const canvas = createCanvas(base.width, base.height);
     const ctx = canvas.getContext('2d');
 
-    const avatar = await loadImage(avatarURL);
+    //const avatar = await loadImage(avatarURL);
 
     ctx.drawImage(base, 0, 0);
-		ctx.drawImage(avatar, 194, 399, 500, 500);
+		//ctx.drawImage(avatar, 194, 399, 500, 500);
     
     console.log(canvas.toDataURL());
     
-    const attachment = new Discord.MessageAttachment('https://i.imgur.com/w3duR07.png');
+    const attachment = new Discord.MessageAttachment(canvas.toDataURL());
 		return message.channel.send(attachment);
   }catch(err) {
     return  message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
