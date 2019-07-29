9004d39ab9f041b7a5fc0f9916365981260bcea3
Renziito <sepia.aki@gmail.com>
2019-07-29 17:57:48 +0000

    💅🕍 Checkpoint ./commands/rip.js:5674830/1452 ./watch.json:5674830/10 ./package.json:5674830/33

--- commands/rip.js
+++ commands/rip.js
@@ -1,34 +1,30 @@
 const Discord = require('discord.js');
 const { createCanvas, loadImage } = require("canvas");
-const request = require('node-superfetch');
 
 module.exports.run = async (bot, message, args) => {
-  let msg = await message.channel.send("Generating avatar...");
+  message.delete();
+  let msg = await message.channel.send("Generando imagen...");
   const mentionedUser = message.mentions.users.first() || message.author;
   const avatarURL = mentionedUser.displayAvatarURL;
   
   try{
-    const canvas = createCanvas(300, 300);
+    const base = await loadImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Frip.png?v=1564422037717");
+    
+    const canvas = createCanvas(base.width, base.height);
     const ctx = canvas.getContext('2d');
 
-    const { rip }  = await request.get(avatarURL)
-    const base = await loadImage(rip);
-
-    const { body } = await request.get(avatarURL);
-    const avatar = await loadImage(body);
+    const avatar = await loadImage(avatarURL);
 
     ctx.drawImage(base, 0, 0);
-    ctx.drawImage(avatar, 194, 399, 500, 500);
-
-    const emb = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setImage(canvas.toBuffer())
-
-    return  message.channel.send(emb)
+		ctx.drawImage(avatar, 194, 399, 500, 500);
+    
+    console.log(canvas.toDataURL());
+    
+    const attachment = new Discord.MessageAttachment('https://i.imgur.com/w3duR07.png');
+		return message.channel.send(attachment);
   }catch(err) {
     return  message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
   }
- 
 }
 module.exports.help = {
   name: "rip"
