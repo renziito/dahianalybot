746f7913c44a4875e301e1766ed7a3d691b15416
Renziito <sepia.aki@gmail.com>
2019-07-29 18:19:50 +0000

    ⚾️⛄️ Checkpoint ./commands/rip.js:5674830/2188 ./watch.json:5674830/4

--- commands/rip.js
+++ commands/rip.js
@@ -10,18 +10,19 @@ module.exports.run = async (bot, message, args) => {
   try{
     const base = await loadImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Frip.png?v=1564422037717");
    
-    const canvas = createCanvas(base.width, base.height);
+    const canvas = createCanvas(700, 250);
     const ctx = canvas.getContext('2d');
 
     //const avatar = await loadImage(avatarURL);
-
-    ctx.drawImage(base, 0, 0);
+    ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(avatarURL, 25, 25, 200, 200);
+    
+    
 		//ctx.drawImage(avatar, 194, 399, 500, 500);
     
-    console.log(canvas.toDataURL());
+    const attachment = new Discord.Attachment(canvas.toBuffer(), 'rip-image.png');
     
-    const attachment = new Discord.MessageAttachment(canvas.toDataURL());
-		return message.channel.send(attachment);
+		return message.channel.send("Se Murio ",mentionedUser,attachment);
   }catch(err) {
     return  message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
   }
