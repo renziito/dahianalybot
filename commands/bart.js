52c7ea4368d3f5042016af56d276ff104eb59595
Renziito <sepia.aki@gmail.com>
2019-08-01 01:20:49 +0000

    ⛺️🌴 Checkpoint ./.glitch-assets:5674830/742 ./commands/bart.js:5674830/5646 ./watch.json:5674830/9

---
+++ commands/bart.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const { createCanvas, loadImage } = require("canvas");
+
+
+module.exports.run = async (bot, message, args) => {
+  let msg = await message.channel.send("Generando imagen...");
+  const mentionedUser = message.mentions.users.first() || message.author;
+  const avatarURL = mentionedUser.displayAvatarURL;
+   
+  try{
+    const base = await loadImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fbart.jpg?v=1564621965776");
+    const overlay = await loadImage("https://cdn.glitch.com/b9b41fa0-8db5-4aa1-a643-fffac74a54f3%2Fbart_2.png?v=1564621974815");
+    const avatar = await loadImage(avatarURL);
+   
+    const canvas = createCanvas(250, 376);
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(overlay, 0, 0, canvas.width, canvas.height);
+    
+    ctx.transform(1, .2, .8, 1, 0, 0);
+    ctx.drawImage(avatar, 40, 60, 100, 100);
+    
+        
+    const attachment = new Discord.Attachment(canvas.toBuffer(), 'rip-image.png');
+    msg.delete();
+
+	  return message.channel.send('', attachment);
+  }catch(err) {
+    return  message.reply(`Oh no, ocurrio un error: \`${err.message}\`. Intentalo después!`);
+  }
+
+  
+}
+module.exports.help = {
+  name: "bart"
+}
