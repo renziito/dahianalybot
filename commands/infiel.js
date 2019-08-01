8a0d44bef71221af1d9cbd5248a9e6dd01b4cb0e
Renziito <sepia.aki@gmail.com>
2019-08-01 03:46:30 +0000

    info y presence

--- commands/infiel.js
+++ commands/infiel.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args) => {
-  message.channel.send('Para mas información haz click ')
+  message.channel.send('🎵 Porque eres un estúpido, Cupido, presumido, torpe que no tienes agallas, que dices mentiras baratas que no sabes como es que se trata a una mujer como yooo... 🎵')
 }
 module.exports.help = {
-  name: "amo"
+  name: "infiel"
 }
